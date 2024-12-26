b5aa1cf1064b6ce9d1ccec17a0926f30ddffc3d5
Sanikit <hunter.w.lawsonii@gmail.com>
2024-12-25 22:09:09 -0600

    Delete dragonball_z_budokai_vegeta_ssgss_playermodel_749314549 directory

--- dragonball_z_budokai_vegeta_ssgss_playermodel_749314549/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-Just put this folder in your garrysmod/addons directory
--- dragonball_z_budokai_vegeta_ssgss_playermodel_749314549/lua/autorun/b3ssj_vgt.lua
+++
@@ -1,2 +0,0 @@
-player_manager.AddValidModel( "SSJ_VGT", "models/player/B3SSJ_VGT/B3SSJ_VGT.mdl" )
-list.Set( "PlayerOptionsModel", "SSJ_VGT", "models/player/B3SSJ_VGT/B3SSJ_VGT.mdl" )
